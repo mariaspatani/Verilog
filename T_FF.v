5acffe38701b57ba6f1008f7cd6ccb82575ba03c
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-10-20 02:20:44 +0530

    T_FF.v

---
+++ T_FF.v
@@ -0,0 +1,15 @@
+module tff(
+    input t,
+    input clk,
+    output reg q,
+    output reg qb
+    );
+    initial q= 1'b0;
+    always@(posedge clk)begin
+    case ({t})
+        1'b0: q=0;
+        1'b1: q=~q;
+    endcase
+        qb = ~q;
+    end
+endmodule
