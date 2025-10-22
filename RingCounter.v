1bbaf8fdd18be5033f4f12f130732add8fcb98cd
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-10-22 22:16:04 +0530

    Implement 4-bit ring counter module

---
+++ RingCounter.v
@@ -0,0 +1,11 @@
+module ring(input clk,output reg[3:0]q
+);
+always @(posedge clk) begin
+case (q)
+4'b1000:q=4'b0100;
+4'b0100:q=4'b0010;
+4'b0010:q=4'b0001;
+4'b0001:q=4'b0000;
+endcase
+end
+endmodule
