b275eac3fd4c5c14a85d9cbcedc830057b2fb3d5
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-10-22 21:56:30 +0530

    Add Johnson counter module in Verilog

---
+++ JohnsonCounter.v
@@ -0,0 +1,17 @@
+
+    module johnson(
+    input clk,output reg[3:0]q
+    );
+    always@(posedge clk)begin
+    case(q)
+        4'b0000:q=4'b1000;
+        4'b1000:q=4'b1100;
+        4'b1100:q=4'b1110;
+        4'b1110:q=4'b1111;
+        4'b1111:q=4'b0111;
+        4'b0111:q=4'b0011;
+        4'b0011:q=4'b0001;
+        4'b0001:q=4'b0000;
+     endcase
+     end
+endmodule
