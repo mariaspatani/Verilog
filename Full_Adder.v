5f917719f600cacc43792c5faedca7386101636d
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-10-23 20:11:57 +0530

    Update Full_Adder.v

--- Full_Adder.v
+++ Full_Adder.v
@@ -10,3 +10,19 @@ module FA(
     assign sum =a^b^c;
     assign carry = (a&b)|(a&c)|(b&c);
 endmodule
+
+//Structural Flow
+module FA(
+    input a,
+    input b,
+    input c,
+    output sum,
+    output carry
+    );
+    wire d,e,f;
+    xor(d,a,b);
+    xor(sum,d,c);
+    and(e,a,b);
+    and(f,c,d);
+    or(carry,e,f);
+endmodule
