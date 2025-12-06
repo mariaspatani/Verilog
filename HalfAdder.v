5ce7126f138788ea432648ccc0076c8ded5bfd0d
Maria S Patani <mariaspatani2028@cs.sjcetpalai.ac.in>
2025-12-06 06:55:51 +0530

    Update HalfAdder.v

--- HalfAdder.v
+++ HalfAdder.v
@@ -8,3 +8,29 @@ module ha(
     xor(sum,a,b);
     and(carry,a,b);
 endmodule
+
+// Dataflow
+module ha(
+    input a,
+    input b,
+    output sum,
+    output carry
+    );
+    assign sum = a^b;
+    assign carry = a&b;
+endmodule
+
+// Behavioral
+module ha(
+    input a,
+    input b,
+    output reg sum,
+    output reg carry
+    );
+    always @( posedge) begin
+        
+    sum = a^b;
+    carry = a&b;
+        
+    end
+endmodule
